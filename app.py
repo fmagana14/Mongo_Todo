08c8e7af204cd88108418c360af4afb2344c8548
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-12-09 16:32:44 -0800

    completed form submit portion

--- app.py
+++ app.py
@@ -1,69 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
-# Remove spaces in the password section of the URI
+# from pymongo import MongoClient
+# The Following code ia my own 
 uri = "mongodb+srv://FMuser:KoolWordz@cluster0.ykezvyd.mongodb.net/?retryWrites=true&w=majority"
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
-db = client["my_database"]
+client = MongoClient('localhost', 27017)
+db = client.flask_db
+todos = db.todos
 
-try:
-    client.server_info()
-    print("Connected to MongoDB successfully!")
-except Exception as e:
-    print(e)
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        content = request.form['content']
+        degree = request.form['priority']
+        todos.insert_one({'content': content, 'priority': degree})
+        return redirect(url_for('index'))
+    
+    # find all todos in mongodb database
+    all_todos_object = list(todos.find())
 
-# Send a ping to confirm a successful connection
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    print(all_todos_object)
 
-@app.route('/mongo_data')
-def get_mongo_data():
-    # Example: Fetch data from MongoDB
-    data_from_mongo = db.my_collection.find_one()
-    return jsonify(data_from_mongo)
+    # give all the todos to the 'index.html' template
+    return render_template('index.html', todos=all_todos_object)
+
+coustom_priority_order ={'Very Important': 1,
+                         'Important': 2, 'Normal': 3, 'Uniportant': 4}
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-from flask import Flask, jsonify
-# from pymongo import MongoClient
-
-
-# app = Flask(__name__)
-
-# uri = "mongodb+srv://FMuser: KoolWordz @cluster0.ykezvyd.mongodb.net/?retryWrites=true&w=majority"
-
-# # Create a new client and connect to the server
-# client = MongoClient(uri)
-# db = client["my_database"]
-# try:
-
-#     client.server_info()
-#     print("Connected to MongoDB successfully!")
-
-# except Exception as e:
-#     print(e)
-# # Send a ping to confirm a successful connection
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
-# @app.route('/mongo_data')
-# def get_mongo_data():
-#     # Example: Fetch data from MongoDB
-#     data_from_mongo = db.my_collection.find_one()
-#     return jsonify(data_from_mongo)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 # KoolWordz
